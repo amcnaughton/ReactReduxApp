ade93b25ea313fe0d00a68af7b0dca52f66eab6a
amcnaughton99 <allanmcnaughton@gmail.com>
2016-11-23 12:20:28 -0500

    More update data

--- src/data/updates.js
+++ src/data/updates.js
@@ -1,18 +1,21 @@
-const updates = [
-  {
-    "comment": "Trail is dry and dusty. Great riding!",
-    "timestamp": 1479393593,
-    "trail_id": 1,
-    "user_id": 23,
-    "status": "open"
-  },
-    {
-    "comment": "Trail is all wet. No riding today!",
-    "timestamp": 1479393595,
-    "trail_id": 1,
-    "user_id": 23,
-    "status": "closed"
-  }
-];
+const updates = [{
+  "comment": "Trail is dry and dusty. Great riding!",
+  "timestamp": 1479393593,
+  "trail_id": 1,
+  "user_id": 23,
+  "status": "open"
+}, {
+  "comment": "Trail is all wet. No riding today!",
+  "timestamp": 1479393595,
+  "trail_id": 1,
+  "user_id": 23,
+  "status": "closed"
+}, {
+  "comment": "Awesome riding today!",
+  "timestamp": 1479393595,
+  "trail_id": 2,
+  "user_id": 23,
+  "status": "open"
+}];
 
 export default updates;
