207410eacc6fc3219ed7ce159738de87cd1d91b5
amcnaughton99 <allanmcnaughton@gmail.com>
2016-11-30 11:14:51 -0500

    Added truncate function

--- src/common.js
+++ src/common.js
@@ -7,8 +7,8 @@
  */
 export function getTrailUpdates(id, updates, count) {
 	return updates
-			.filter(item => item.trail_id === +id)
-			.sort((a, b) => b.timestamp - a.timestamp).slice(0, count);
+		.filter(item => item.trail_id === +id)
+		.sort((a, b) => b.timestamp - a.timestamp).slice(0, count);
 }
 
 /**
@@ -20,7 +20,7 @@ export function getTrailUpdates(id, updates, count) {
  */
 export function getTrailIndex(trailId, trails) {
 	return trails
-			.map(trail => trail.id).indexOf(+trailId);
+		.map(trail => trail.id).indexOf(+trailId);
 }
 
 /**
@@ -37,4 +37,27 @@ export function isTrailOpen(id, updates) {
 		.sort((a, b) => b.timestamp - a.timestamp);
 	console.log("getTrailUpdates", id, results)
 	return results
+}
+
+/**
+ * Shorten a string with ...
+ * 
+ * @param {any} str
+ * @param {any} n
+ * @param {any} useWordBoundary
+ * @returns string
+ */
+export function truncate(str, n, useWordBoundary) {
+	var singular, tooLong = str.length > n;
+	useWordBoundary = useWordBoundary || true;
+
+	// Edge case where someone enters a ridiculously long string.
+	str = tooLong ? str.substr(0, n - 1) : str;
+
+	singular = (str.search(/\s/) === -1) ? true : false;
+	if (!singular) {
+		str = useWordBoundary && tooLong ? str.substr(0, str.lastIndexOf(' ')) : str;
+	}
+
+	return tooLong ? str + '...' : str;
 }
