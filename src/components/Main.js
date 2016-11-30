12bd60a5a22955ce9dbab51dcd1f7189a13dd37f
amcnaughton99 <allanmcnaughton@gmail.com>
2016-11-30 11:15:58 -0500

    Removed Boostrap dependency

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { Grid } from 'react-bootstrap';
-
 import Header from './Header';
 import Footer from './Footer';
 
@@ -9,13 +7,11 @@ class Main extends React.Component {
 
   render() {
     return (
-    	<Grid>
-	      <div>
-	        <Header/>
-	        {React.cloneElement({...this.props}.children, {...this.props})}
-	        <Footer/>
-	      </div>
-      </Grid>
+	    <div>
+	      <Header/>
+	      {React.cloneElement({...this.props}.children, {...this.props})}
+	      <Footer/>
+	    </div>
     )
   }
 }
