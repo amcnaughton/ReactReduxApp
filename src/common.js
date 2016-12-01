c057b9b5b72d7447edad0898be22d46bcd63a329
amcnaughton99 <allanmcnaughton@gmail.com>
2016-12-01 09:15:19 -0500

    Use default arguments

--- src/common.js
+++ src/common.js
@@ -1,23 +1,21 @@
 // Return sorted updates for selected trail
-export function getTrailUpdates(id, updates, count) {
+export function getTrailUpdates(id, updates = [], count) {
 	return updates
 		.filter(item => item.trail_id === +id)
 		.sort((a, b) => b.timestamp - a.timestamp).slice(0, count);
 }
 
 // Map trailId to array index
-export function getTrailIndex(trailId, trails) {
+export function getTrailIndex(trailId, trails = []) {
 	return trails
 		.map(trail => trail.id).indexOf(+trailId);
 }
 
 // The most recent trail update contains the trail open/closed status
-export function isTrailOpen(id, updates) {
-
+export function isTrailOpen(id, updates = []) {
 	var results = updates
 		.filter(item => item.trail_id === +id)
 		.sort((a, b) => b.timestamp - a.timestamp);
-	console.log("getTrailUpdates", id, results)
 	return results
 }
 
