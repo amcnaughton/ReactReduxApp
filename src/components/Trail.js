01062c19e0d8dbb1307bacbc94823f21ce84e106
amcnaughton99 <allanmcnaughton@gmail.com>
2016-12-01 07:55:34 -0500

    Fixed links

--- src/components/Trail.js
+++ src/components/Trail.js
@@ -10,31 +10,27 @@ class Trail extends React.Component {
 		const {trail} = this.props;
 
 		return (
-			<div className="clearfix trail-summary">	
+			<div className="clearfix trail-summary">
 				<div className="trail-header">
 					{trail.name}
-					<Likes trailId={+trail.id} trail={trail} increment={this.props.increment}/>
-					<Link to={`/trail/${trail.id}`}><span className="details-button"/></Link>
+					<Likes trailId={+trail.id} trail={trail} increment={this.props.increment} />
+					<Link to={`/trail/${trail.id}`}><span className="details-button" /></Link>
 				</div>
-			  <div className="trail-image">
+				<div className="trail-image">
 					<Link to={`/trail/${trail.id}`}>
-						<img src={`/files/${trail.image}`}/>	
+						<img src={`/files/${trail.image}`} />
 					</Link>
 				</div>
 				<div className="trail-description">{trail.description}</div>
 
-      	<Updates trailId={String(trail.id)} count={1} hideForm={true} {...this.props}/>
+				<Updates trailId={String(trail.id)} count={1} hideForm={true} {...this.props} />
 
 				<div className="link-panel">
 					<div className="update-link">
-						<Link to={`/trail/${trail.id}`}>
-							<div className="update-button"/><span>Update Status</span>
-						</Link>
+						<Link to={`/trail/${trail.id}`}>Update Status</Link>
 					</div>
 					<div className="directions-link">
-						<Link to={`/trail/${trail.id}`}>
-							<div className="directions-button"/>Directions
-						</Link>
+						<Link to={`/trail/${trail.id}`}>Directions</Link>
 					</div>
 				</div>
 
