e52ab150789aebcbed9e87606d9612a85836b1d7
amcnaughton99 <allanmcnaughton@gmail.com>
2016-12-01 09:48:07 -0500

    More data

--- src/data/trails.js
+++ src/data/trails.js
@@ -1,17 +1,40 @@
-const trails = [{
-  "description": "Great beginner mountain biking",
+const trails = [
+{
+  "description": "By far the most popular beginner friendly singletrack riding in the Triangle. Centrally located near RDU.",
   "id": 1,
   "name": "Crabtree",
   "location": "Lake Crabtree County Park,NC",
-  "status": "open",
-  "likes": 23
+  "likes": 23,
+  "image": "crabtree.jpg"
 }, {
-  "description": "Great mountain biking adjacent to Harris Lake",
+  "description": "The major loops here are signed as 'beginner', 'intermediate' and 'advanced', and can be enjoyed by all skill levels.",
   "id": 2,
   "name": "Harris Lake",
   "location": "Harris Lake County Park,NC",
-  "status": "closed",
-  "likes": 44
-}];
+  "likes": 44,
+  "image": "harrislake.jpg"
+}, {
+  "description": "The trails include three loops: a 2.6-mile outer loop, a 1.6-mile inner loop, a 2.4-mile west loop and more advanced 8.5-mile loop called South Loop.",
+  "id": 3,
+  "name": "Beaver Dam",
+  "location": "Beaver Dam State Recreation Area, NC",
+  "likes": 15,
+  "image": "beaverdam.jpg"
+}, {
+  "description": "A beautiful mix of tech and flow. Littered with roots, rocks, logs, and dips. There are several rock gardens to hone your skills on.",
+  "id": 4,
+  "name": "New Light",
+  "location": "1505 Old Weaver Trail, Wake Forest, NC 27587",
+  "likes": 29,
+  "image": "newlight.jpg"
+}, {
+  "description": "A wide variety of trail terrain and features. Physically fit intermediate-advanced with some expert sections. Not recommended for beginners.",
+  "id": 5,
+  "name": "Sanlee",
+  "location": "572 Pumping Station Rd, Sanford, NC 27330",
+  "likes": 42,
+  "image": "sanlee.jpg"
+}
+];
 
 export default trails;
